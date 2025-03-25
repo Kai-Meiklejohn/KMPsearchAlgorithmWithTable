0559b1551cf13f75ea7d2b06a3ee2c04adb6aadb
Kai Meiklejohn <kaim.meiklejohn@gmail.com>
2025-03-25 21:42:38 +1300

    implemented the kmpsearch args check and method and class calling

--- KMPsearch.java
+++ KMPsearch.java
@@ -5,5 +5,23 @@
 // Solo project
 
 public class KMPsearch {
-    
+    public static void main(String[] args) {
+        // check how many args we got
+        if (args.length == 1) {
+            String target = args[0];
+            // just one arg means skip table time
+            SkipTable skipTable = new SkipTable(target);
+            skipTable.printSkipTable();
+        } else if (args.length == 2) {
+            // two args, so we are searching a file
+            String target = args[0];
+            String filename = args[1];
+            FileProcessor fileProcessor = new FileProcessor(filename, target);
+            // solo mode, first hit only
+            fileProcessor.processFile(false);
+        } else {
+            // throw error to useer
+            System.out.println("Usage: java KMPsearch \"target\" [filename.txt]");
+        }
+    }
 }

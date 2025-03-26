0a12859e0eb85981ec13ca5565e51d8142562bf6
Kai Meiklejohn <kaim.meiklejohn@gmail.com>
2025-03-26 21:53:15 +1300

    changed how to get lps table

--- SkipTable.java
+++ SkipTable.java
@@ -4,6 +4,7 @@
 // ID: 1632448
 // Solo project
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
@@ -14,32 +15,90 @@ public class SkipTable {
 
     public SkipTable(String pattern) {
         this.pattern = pattern;
-        this.lps = computeLPSArray();
+        this.lps = new int[this.pattern.length()];
+        constructLps(this.pattern, this.lps);
     }
 
-    // Compute the LPS array for the pattern
-    private int[] computeLPSArray() {
-        int[] lps = new int[pattern.length()];
-        int length = 0; // Length of the previous longest prefix suffix
+    static void constructLps(String pat, int[] lps) {
+        
+        // len stores the length of longest prefix which 
+        // is also a suffix for the previous index
+        int len = 0;
+
+        // lps[0] is always 0
+        lps[0] = 0;
+
         int i = 1;
-
-        lps[0] = 0; // First position has no prefix/suffix
-
-        while (i < pattern.length()) {
-            if (pattern.charAt(i) == pattern.charAt(length)) {
-                length++;
-                lps[i] = length;
+        while (i < pat.length()) {
+            
+            // If characters match, increment the size of lps
+            if (pat.charAt(i) == pat.charAt(len)) {
+                len++;
+                lps[i] = len;
                 i++;
-            } else {
-                if (length != 0) {
-                    length = lps[length - 1];
-                } else {
+            }
+            
+            // If there is a mismatch
+            else {
+                if (len != 0) {
+                    
+                    // Update len to the previous lps value 
+                    // to avoid redundant comparisons
+                    len = lps[len - 1];
+                } 
+                else {
+                    
+                    // If no matching prefix found, set lps[i] to 0
                     lps[i] = 0;
                     i++;
                 }
             }
         }
-        return lps;
+    }
+
+    static ArrayList<Integer> search(String pat, String txt) {
+        int n = txt.length();
+        int m = pat.length();
+
+        int[] lps = new int[m];
+        ArrayList<Integer> res = new ArrayList<>();
+
+        constructLps(pat, lps);
+
+        // Pointers i and j, for traversing 
+        // the text and pattern
+        int i = 0;
+        int j = 0;
+
+        while (i < n) {
+            // If characters match, move both pointers forward
+            if (txt.charAt(i) == pat.charAt(j)) {
+                i++;
+                j++;
+
+                // If the entire pattern is matched 
+                // store the start index in result
+                if (j == m) {
+                    res.add(i - j);
+                    
+                    // Use LPS of previous index to 
+                    // skip unnecessary comparisons
+                    j = lps[j - 1];
+                }
+            }
+            
+            // If there is a mismatch
+            else {
+                
+                // Use lps value of previous index
+                // to avoid redundant comparisons
+                if (j != 0)
+                    j = lps[j - 1];
+                else
+                    i++;
+            }
+        }
+        return res;
     }
 
     // Print the skip table as per assignment specification
